0ef1c9f2c678e5ac2baac4bf6e84ad3fa60ea2db
thanhnc <thanhnc138@mail.com>
2021-04-21 18:20:47 +0700

    update bl backtest

--- application/api/backtest/BLBacktest.py
+++ application/api/backtest/BLBacktest.py
@@ -11,16 +11,29 @@ from application.api.backtest.strategies.TestStrategy import TestStrategy
 import pandas as pd
 from application.utility.datetime_utils import subtract_days, is_weekday, parse_date, format_date, get_yesterday_weekday, get_the_day_before_yesterday_weekday
 import importlib.util
+import backtrader.analyzers as btanalyzers
 
 def backtest_strategy(config):
+    result = {}
+    # cerebro
     cerebro = prepare_cerebro(config)
-    data = prepare_feed_data(config)
-    cerebro.adddata(data)
-    strategy_config = prepare_strategy(config)
-    strategy = strategy_config.get('strategy')
-    params = strategy_config.get('params')
-    cerebro.addstrategy(strategy, **params)
-    result = run_backtest(cerebro)
+    # data
+    df = prepare_feed_data(config)
+    if len(df) > 0:
+        data = bt.feeds.PandasData(dataname=df)
+        cerebro.adddata(data)
+        # strategy_config
+        strategy_config = prepare_strategy(config)
+        # analyzer
+        cerebro = add_analyzers(cerebro)
+        # strategy
+        strategy = strategy_config.get('strategy')
+        params = strategy_config.get('params')
+        cerebro.addstrategy(strategy, **params)
+        # run
+        strats = run_backtest(cerebro)
+        # result
+        result = get_trade_result(strats, cerebro)
     return result
 
 def prepare_cerebro(config):
@@ -30,6 +43,27 @@ def prepare_cerebro(config):
     # Set the commission - 0.1% ... divide by 100 to remove the %
     commission = config.get('commission') or 0.001
     cerebro.broker.setcommission(commission=commission)
+    # Add a FixedSize sizer according to the stake
+    # cerebro.addsizer(bt.sizers.FixedSize, stake=5)
+    return cerebro
+
+def add_analyzers(cerebro):
+    cerebro.addanalyzer(btanalyzers.AnnualReturn, _name='annual_return')
+    cerebro.addanalyzer(btanalyzers.TradeAnalyzer, _name='trade_analyzer')
+
+    cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='sharpe_ratio')
+    cerebro.addanalyzer(btanalyzers.SharpeRatio_A, _name='sharpe_ratio_a') # sharpe ratio anualized
+    cerebro.addanalyzer(btanalyzers.DrawDown, _name='draw_down')
+    cerebro.addanalyzer(btanalyzers.Returns, _name='returns')
+    cerebro.addanalyzer(btanalyzers.Transactions, _name='transactions')
+    cerebro.addanalyzer(btanalyzers.SQN, _name='sqn')
+    cerebro.addanalyzer(btanalyzers.TimeDrawDown, _name='time_drawdown')
+    cerebro.addanalyzer(btanalyzers.PyFolio, _name='pyfolio')
+    cerebro.addanalyzer(btanalyzers.VWR, _name='vwr') # sharpe ratio with log returns
+    cerebro.addanalyzer(btanalyzers.Calmar, _name='calmar')
+    cerebro.addanalyzer(btanalyzers.LogReturnsRolling, _name='log_returns_rolling')
+    cerebro.addanalyzer(btanalyzers.PeriodStats, _name='periods_stats')
+    cerebro.addanalyzer(btanalyzers.TimeReturn, _name='time_return')
     return cerebro
 
 def prepare_feed_data(config):
@@ -57,8 +91,9 @@ def prepare_feed_data(config):
         }
     sql_data = list(map(map_row_to_candlestick, sql_data))
     df = pd.DataFrame(sql_data)
-    df.set_index('Date', inplace=True)
-    return bt.feeds.PandasData(dataname=df)
+    if len(sql_data) > 0:
+        df.set_index('Date', inplace=True)
+    return df
 
 def prepare_strategy(config):
     params = {}
@@ -81,8 +116,78 @@ def get_strategy_config(strategy_name):
     }
 
 def run_backtest(cerebro):
-    result = []
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    cerebro.run()
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    return result
+    # print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    strats = cerebro.run()
+    # print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    return strats
+
+def get_trade_result(strats, cerebro):
+    _show_analyzers_end(strats[0])
+    final_portfolio = round(cerebro.broker.getvalue(), 2)
+    if strats[0] is not None:
+        result = strats[0].get_trade_result()
+        win_rate = strats[0].get_win_rate()
+        total_trades = strats[0].get_total_trades()
+        analyzer = strats[0].analyzers.trade_analyzer.get_analysis()
+        total_open = analyzer.total.open
+        total_closed = analyzer.total.closed
+        total_won = analyzer.won.total
+        total_lost = analyzer.lost.total
+        pnl = round(analyzer.pnl.net.total, 2)
+        percent_pnl = round(pnl / (final_portfolio - pnl) * 100, 2)
+    return {
+        'result': result,
+        'total_trades': total_trades,
+        # 'total_open': total_open,
+        # 'total_closed': total_closed,
+        # 'total_won': total_won,
+        # 'total_lost': total_lost,
+        'win_rate': win_rate,
+        # 'analyzer': analyzer,
+        'pnl': pnl,
+        'percent_pnl': percent_pnl,
+        'final_portfolio': final_portfolio
+    }
+
+def _show_analyzers_end(strats):
+    # print('Annual Return: ' + str(strats.analyzers.annual_return.get_analysis()))
+    # print('TradeAnalyzer: ' + str(strats.analyzers.trade_analyzer.get_analysis()))
+    _printTradeAnalysis(strats.analyzers.trade_analyzer.get_analysis())
+
+    # print('Returns: ' + str(strats.analyzers.returns.get_analysis()))
+    # print('Transactions: ' + str(strats.analyzers.transactions.get_analysis()))
+    # print('Sharpe Ratio: ' + str(strats.analyzers.sharpe_ratio.get_analysis()))
+    # print('Sharpe Ratio Annualized: ' + str(strats.analyzers.sharpe_ratio_a.get_analysis())) # sharpe ratio anualized
+    # print('Sharpe Ratio Log Returns: ' + str(strats.analyzers.vwr.get_analysis())) # sharpe ratio with log returns
+    # print('Draw Down: ' + str(strats.analyzers.draw_down.get_analysis()))
+    # print('SQN: ' + str(strats.analyzers.sqn.get_analysis()))
+    # print('Time Drawdown: ' + str(strats.analyzers.time_drawdown.get_analysis()))
+    # print('Time Return: ' + str(strats.analyzers.time_return.get_analysis()))
+    # print('Calmar: ' + str(strats.analyzers.calmar.get_analysis()))
+    # print('Log Returns Rolling: ' + str(strats.analyzers.log_returns_rolling.get_analysis()))
+    # print('Periods Stats: ' + str(strats.analyzers.periods_stats.get_analysis()))
+    # print('PyFolio: ' + str(strats.analyzers.pyfolio.get_analysis()))
+
+def _printTradeAnalysis(analyzer):
+    '''
+    Function to print the Technical Analysis results in a nice format.
+    https://backtest-rookies.com/2017/06/11/using-analyzers-backtrader/
+    '''
+    # Get the results we are interested in
+    total_open = analyzer.total.open
+    total_closed = analyzer.total.closed
+    total_won = analyzer.won.total
+    total_lost = analyzer.lost.total
+
+    # Designate the rows
+    h1 = ['Total Open', 'Total Closed', 'Total Won', 'Total Lost']
+    r1 = [total_open, total_closed,total_won,total_lost]
+
+    header_length = len(h1)
+
+    # Print the rows
+    print_list = [h1, r1]
+    row_format ="{:<15}" * (header_length + 1)
+    print("Trade Analysis Results:")
+    for row in print_list:
+        print(row_format.format('', *row))
--- application/api/backtest/strategies/BaseStrategy.py
+++ application/api/backtest/strategies/BaseStrategy.py
@@ -15,10 +15,27 @@ class BaseStrategy(bt.Strategy):
 
         self.orefs = None
 
+        self.trade_result = []
+        self.win_quantity = 0
+        self.loss_quantity = 0
+
     def log(self, txt, dt=None):
         ''' Logging function fot this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
+
+    def get_date_format(self):
+        return self.datas[0].datetime.date(0).isoformat()
+
+    def get_trade_result(self):
+        return self.trade_result
+
+    def get_win_rate(self):
+        total_trades = self.win_quantity + self.loss_quantity or 1
+        return round(self.win_quantity / (total_trades) * 100, 2)
+
+    def get_total_trades(self):
+        return self.win_quantity + self.loss_quantity
 
     def next(self):
         # from settings import CONFIG
@@ -51,7 +68,12 @@ class BaseStrategy(bt.Strategy):
                                            valid=valid1,
                                            transmit=False,
                                            size=CONFIG['size'])
-            self.log('BUY CREATE, %.2f' % p1)
+            # self.log('BUY CREATE, %.2f' % p1)
+            self.trade_result.append({
+                'transaction_type': 'BUY CREATE',
+                'transaction_date': self.get_date_format(),
+                'price': p1
+            })
             aux_orefs.append(o1.ref)
 
             if CONFIG['stop_loss']['enabled']:
@@ -61,7 +83,12 @@ class BaseStrategy(bt.Strategy):
                                                 parent=o1,
                                                 transmit=False,
                                                 size=CONFIG['size'])
-                self.log('STOP LOSS CREATED, %.2f' % p2)
+                # self.log('STOP LOSS CREATED, %.2f' % p2)
+                self.trade_result.append({
+                    'transaction_type': 'STOP LOSS CREATED',
+                    'transaction_date': self.get_date_format(),
+                    'price': p2
+                })
                 aux_orefs.append(o2.ref)
 
             if CONFIG['take_profit']['enabled']:
@@ -71,13 +98,23 @@ class BaseStrategy(bt.Strategy):
                                                 parent=o1,
                                                 transmit=True,
                                                 size=CONFIG['size'])
-                self.log('TAKE PROFIT CREATED, %.2f' % p3)
+                # self.log('TAKE PROFIT CREATED, %.2f' % p3)
+                self.trade_result.append({
+                    'transaction_type': 'TAKE PROFIT CREATED',
+                    'transaction_date': self.get_date_format(),
+                    'price': p3
+                })
                 aux_orefs.append(o3.ref)
 
             self.orefs = aux_orefs
 
         else:
-            self.log('BUY CREATE, %.2f' % self.dataclose[0])
+            # self.log('BUY CREATE, %.2f' % self.dataclose[0])
+            self.trade_result.append({
+                'transaction_type': 'BUY CREATE',
+                'transaction_date': self.get_date_format(),
+                'price': self.dataclose[0]
+            })
             self.order = super(BaseStrategy, self).buy(size=CONFIG['size'])
 
     def sell(self):
@@ -85,7 +122,12 @@ class BaseStrategy(bt.Strategy):
         from application.api.backtest.settings import CONFIG
 
         if not CONFIG['take_profit']['enabled'] and not CONFIG['stop_loss']['enabled']:
-            self.log('SELL CREATE, %.2f' % self.dataclose[0])
+            # self.log('SELL CREATE, %.2f' % self.dataclose[0])
+            self.trade_result.append({
+                'transaction_type': 'SELL CREATE',
+                'transaction_date': self.get_date_format(),
+                'price': self.dataclose[0]
+            })
             self.order = super(BaseStrategy, self).sell(size=CONFIG['size'])
 
     def notify_order(self, order):
@@ -100,20 +142,32 @@ class BaseStrategy(bt.Strategy):
         # Attention: broker could reject order if not enougth cash
         if order.status in [order.Completed, order.Canceled, order.Margin]:
             if order.isbuy():
-                self.log(
-                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                    (order.executed.price,
-                     order.executed.value,
-                     order.executed.comm))
-
+                # self.log(
+                #     'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                #     (order.executed.price,
+                #      order.executed.value,
+                #      order.executed.comm))
+                self.trade_result.append({
+                    'transaction_type': 'BUY EXECUTED',
+                    'transaction_date': self.get_date_format(),
+                    'price': order.executed.price,
+                    'cost': order.executed.value,
+                    'commission': order.executed.comm,
+                })
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
             else:  # Sell
-                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                         (order.executed.price,
-                          order.executed.value,
-                          order.executed.comm))
-
+                # self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                #          (order.executed.price,
+                #           order.executed.value,
+                #           order.executed.comm))
+                self.trade_result.append({
+                    'transaction_type': 'SELL EXECUTED',
+                    'transaction_date': self.get_date_format(),
+                    'price': order.executed.price,
+                    'cost': order.executed.value,
+                    'commission': order.executed.comm,
+                })
             self.bar_executed = len(self)
 
         if CONFIG['stop_loss']['enabled'] or CONFIG['take_profit']['enabled']:
@@ -127,5 +181,15 @@ class BaseStrategy(bt.Strategy):
         if not trade.isclosed:
             return
 
-        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
-                 (trade.pnl, trade.pnlcomm))
+        # self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+        #          (trade.pnl, trade.pnlcomm))
+        self.trade_result.append({
+            'transaction_type': 'OPERATION PROFIT',
+            'transaction_date': self.get_date_format(),
+            'gross': trade.pnl,
+            'net': trade.pnlcomm
+        })
+        if trade.pnlcomm >= 0:
+            self.win_quantity += 1
+        else:
+            self.loss_quantity += 1
