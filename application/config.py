9146fd39d7eff537adde35cf5dbadfee9c72eed1
thanhnc <thanhnc138@mail.com>
2021-06-15 18:29:12 +0700

    update config

--- application/config.py
+++ application/config.py
@@ -9,21 +9,21 @@ class Config(object):
 
 
 class ProductionConfig(Config):
+    """Production configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_POSTGRESQL")
 
 
 class DevelopmentConfig(Config):
     """Development configuration"""
-    user = os.environ["POSTGRES_USER"]
-    password = os.environ["POSTGRES_PASSWORD"]
-    hostname = os.environ["POSTGRES_HOSTNAME"]
-    port = os.environ["POSTGRES_PORT"]
-    database = os.environ["APPLICATION_DB"]
+    # user = os.environ["POSTGRES_USER"]
+    # password = os.environ["POSTGRES_PASSWORD"]
+    # hostname = os.environ["POSTGRES_HOSTNAME"]
+    # port = os.environ["POSTGRES_PORT"]
+    # database = os.environ["APPLICATION_DB"]
 
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{database}"
-    )
-    """Production configuration"""
+    # SQLALCHEMY_DATABASE_URI = (
+    #     f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{database}"
+    # )
 
 
 class TestingConfig(Config):
