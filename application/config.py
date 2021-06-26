512b18cda3d6e17680bdb86a4f51182428b6debb
thanhnc <thanhnc138@mail.com>
2021-06-26 14:43:32 +0700

    update db config

--- application/config.py
+++ application/config.py
@@ -25,6 +25,11 @@ class DevelopmentConfig(Config):
     #     f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{database}"
     # )
 
+    # SQLALCHEMY_DATABASE_URI = (
+    #     f"postgresql+psycopg2://ogwsmojrynkhmc:5e3f5b888472b53141e050ea3e366765203feab53b43a13248b65f718e477e63@ec2-52-86-2-228.compute-1.amazonaws.com:5432/dg0q8kfl0thi4"
+    # )
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_POSTGRESQL")
 
 class TestingConfig(Config):
     """Testing configuration"""
